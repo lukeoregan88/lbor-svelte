5da18862071530399fd7739cecbd2be2a7fdc5e1
Luke O'Regan <luke@dda.co.uk>
2023-02-01 22:08:59 +0000

    Setup pages ready for site

---
+++ src/components/header.svelte
@@ -0,0 +1,14 @@
+<header>
+  Header
+  <div>
+    logo
+  </div>
+  <div>
+    <nav class="main-menu_nav"> 
+      <a rel="about" href="/about/">About</a> 
+      <a rel="skills" href="/skills/">My Skills</a> 
+      <a rel="work" href="/portfolio/">Work</a> 
+      <a rel="contact" href="/contact/">Contact</a> 
+      <a style="color: #08FDD8;" href="/blog/">Blog</a></nav>
+  </div>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
   import "../app.scss";
+  import Header from "../components/header.svelte";
 </script>
+
+<Header />
 
 <slot />
