1996965dd81b2d19833abc8bbaefb516bd163227
Luke O'Regan <luke@dda.co.uk>
2023-02-05 17:28:10 +0000

    Changed posts URL

--- src/routes/blog/+page.js
+++ src/routes/blog/+page.js
@@ -1,7 +1,7 @@
 /** @format */
 
 export const load = async ({ fetch }) => {
-  const res = await fetch("https://blog.lbor.dev/wp-json/wp/v2/posts");
+  const res = await fetch("https://blog.lbor.co.uk/wp-json/wp/v2/posts");
   const allPosts = await res.json();
   const posts = allPosts.slice(0, 10);
 
--- src/routes/blog/[slug]/+page.js
+++ src/routes/blog/[slug]/+page.js
@@ -4,7 +4,7 @@ import { error } from "@sveltejs/kit";
 
 export const load = async ({ fetch, params: { slug } }) => {
   const res = await fetch(
-    `https://blog.lbor.dev/wp-json/wp/v2/posts?slug=${slug}`
+    `https://blog.lbor.co.uk/wp-json/wp/v2/posts?slug=${slug}`
   );
   const post = await res.json();
 
